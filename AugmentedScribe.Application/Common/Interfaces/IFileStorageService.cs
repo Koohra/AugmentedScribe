392d88e95e3ae4f6d962f623e261f3e7635a8e8e
Miguel Pereira <miguel.pereira.12@live.com>
2025-10-29 20:33:34 -0300

    feat(books): implement delete book

--- AugmentedScribe.Application/Common/Interfaces/IFileStorageService.cs
+++ AugmentedScribe.Application/Common/Interfaces/IFileStorageService.cs
@@ -3,4 +3,5 @@ namespace AugmentedScribe.Application.Common.Interfaces;
 public interface IFileStorageService
 {
     Task<string> UploadFileAsync(Stream fileStream, string blobName, CancellationToken cancellationToken = default);
+    Task DeleteFileAsync(string blobName, CancellationToken cancellationToken = default);
 }
